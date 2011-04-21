f3826362cbbcb0110f9e08269e78e42cb34c8b35
Ammar Qammaz <ammarkov@gmail.com>
2011-04-22 02:39:44 +0300

    integral images commit

---
+++ VisualCortex/IntegralImageConversion.h
@@ -0,0 +1,14 @@
+#ifndef INTEGRALIMAGECONVERSION_H_INCLUDED
+#define INTEGRALIMAGECONVERSION_H_INCLUDED
+
+#include "VisionMemory.h"
+
+inline void GenerateCompressHistogramOfImage_AddUpHorizontal(unsigned char * input_img,unsigned short * output_img,unsigned int block_x,unsigned int block_y);
+inline void GenerateCompressHistogramOfImage_AddUpVertical(unsigned short * output_img,unsigned int block_x,unsigned int block_y);
+void GenerateCompressHistogramOfImage(unsigned char * input_img,unsigned short * output_img,unsigned int block_x,unsigned int block_y);
+unsigned int CompressedHistogramPatch(unsigned short * compimg,struct Histogram * hist , unsigned int x,unsigned int y);
+
+int GetCompressedRegisterPatchSum1Byte(int comp_register,int x,int y,int width,int height);
+int CompressRegister1Byte(int input,int output);
+
+#endif // INTEGRALIMAGECONVERSION_H_INCLUDED
