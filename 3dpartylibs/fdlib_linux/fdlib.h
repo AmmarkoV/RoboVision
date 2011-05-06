0080ad1f446b47ac3bb6cb23f7bcb5489a5ebdd9
Ammar Qammaz <ammarkov@gmail.com>
2011-05-06 22:27:27 +0300

    Removing FDlib :) , I will not use it..! , also consoleout.dat should not be commited every time so i am emptying it

--- 3dpartylibs/fdlib_linux/fdlib.h
+++
@@ -1,4 +0,0 @@
-void fdlib_detectfaces(unsigned char *rgbimage, int imagewidth, int imageheight, int threshold);
-int fdlib_getndetections();
-void fdlib_getdetection(int nr, int *xpos, int *ypos, int *width);
-
