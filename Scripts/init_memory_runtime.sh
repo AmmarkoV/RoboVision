6f692cf1af40f575f44e7b4e68fe977ab7bd1495
Ammar Qammaz <ammarkov@gmail.com>
2011-09-25 01:50:58 +0300

    ...

--- Scripts/init_memory_runtime.sh
+++ Scripts/init_memory_runtime.sh
@@ -18,13 +18,15 @@ mkdir ../RoboVisionRuntime/memfs/public_html
 
 
 echo "Copy Files to run!"
-cp ../RoboVisionCLI/empty.jpeg RoboVisionRuntime/empty.jpeg
-cp ../RoboVisionCLI/empty.ppm RoboVisionRuntime/empty.ppm
-cp ../RoboVisionCLI/guard.ini RoboVisionRuntime/guard.ini
-cp ../RoboVisionCLI/RoboVisionCLICompat RoboVisionRuntime/RoboVisionCLICompat
-cp ../RoboVisionCLI/bin/Release/RoboVisionCLI RoboVisionRuntime/bin/Release/RoboVisionCLI 
-cp ../RoboVisionCLI/scripts/* RoboVisionRuntime/scripts/
-cp ../RoboVisionCLI/Sounds/* RoboVisionRuntime/Sounds/
+
+cp ../RoboVisionCLI/haarcascade_frontalface_alt.xml ../RoboVisionRuntime/haarcascade_frontalface_alt.xml
+cp ../RoboVisionCLI/empty.jpeg ../RoboVisionRuntime/empty.jpeg
+cp ../RoboVisionCLI/empty.ppm ../RoboVisionRuntime/empty.ppm
+cp ../RoboVisionCLI/guard.ini ../RoboVisionRuntime/guard.ini
+cp ../RoboVisionCLI/RoboVisionCLICompat ../RoboVisionRuntime/RoboVisionCLICompat
+cp ../RoboVisionCLI/bin/Release/RoboVisionCLI ../RoboVisionRuntime/bin/Release/RoboVisionCLI 
+cp ../RoboVisionCLI/scripts/* ../RoboVisionRuntime/scripts/
+cp ../RoboVisionCLI/Sounds/* ../RoboVisionRuntime/Sounds/
 
 echo "everything done!"
 
