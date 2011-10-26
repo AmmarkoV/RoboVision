754d85c77cb34e56aff7cdab49918abe74fe6fca
Ammar Qammaz <ammarkov@gmail.com>
2011-10-27 02:01:20 +0300

    adding the last piece of the robovision puzzle , the place that will hold the 3D map and that will be the missing link that will connect RouteFinding Disparity Mapping Pose Tracking , etc.

---
+++ WorldMapping/MasterWorld/MasterWorld.h
@@ -0,0 +1,20 @@
+#ifndef MASTERWORLD_H_INCLUDED
+#define MASTERWORLD_H_INCLUDED
+
+struct Points
+{
+   unsigned char r,g,b;
+   unsigned char exists;
+   unsigned char size;
+   float x,y,z;
+};
+
+
+struct Agents
+{
+   float x,y,z;
+};
+
+
+
+#endif // MASTERWORLD_H_INCLUDED
---
+++ WorldMapping/MasterWorld/main.c
@@ -0,0 +1,23 @@
+#include "MasterWorld.h"
+
+#define MAX_POINTS_ON_WORLD 4096
+#define MAX_AGENTS_ON_WORLD 2
+
+struct Points voxels[MAX_POINTS_ON_WORLD]={0};
+struct Agents agent[MAX_AGENTS_ON_WORLD]={0};
+unsigned int total_points_number=0;
+
+
+
+
+int Add3DPoint(float x,float y,float z , unsigned char r ,unsigned char g ,unsigned char b)
+{
+
+}
+
+
+
+int SetAgent(unsigned int agent_num,float x,float y,float z , unsigned char r ,unsigned char g ,unsigned char b)
+{
+
+}
