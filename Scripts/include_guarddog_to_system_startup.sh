0c4c90bffa0fbaa19f4fd4bd696bce2c2b915234
Ammar Qammaz <ammarkov@gmail.com>
2011-11-11 05:15:05 +0200

    set up guarddog to act as a dhcp server , also changed the kernel module to madwifi , added some memo scripts , generally wireless network work

--- Scripts/include_guarddog_to_system_startup.sh
+++ Scripts/include_guarddog_to_system_startup.sh
@@ -15,4 +15,47 @@ sudo cp Scripts/guarddog_linux_startup_script /etc/init.d/guarddog_linux_startup
 sudo chmod 755 /etc/init.d/guarddog_linux_startup_script
 sudo chmod +x /etc/init.d/guarddog_linux_startup_script
 sudo update-rc.d guarddog_linux_startup_script defaults 
+
+
+
+
+echo "The next segment prepares dhcp server for a 192.168.1.0-255 range  ..!"
+echo
+echo -n "                Do you want to proceed (Y/N)?"
+read answer
+if test "$answer" != "Y" -a "$answer" != "y";
+then exit 0;
+fi
+ 
+
+sudo apt-get install dhcp3-server  
+sudo cp Scripts/dhcpd.conf /etc/dhcp3/dhcpd.conf
+
+
+echo "The next segment gives guarddog an IP of 192.168.1.14 and gateway 192.168.1.1 on ethernet ..!"
+echo "And puts it on a wireless access point mode !"
+echo
+echo -n "                Do you want to proceed (Y/N)?"
+read answer
+if test "$answer" != "Y" -a "$answer" != "y";
+then exit 0;
+fi
+ 
+ 
+sudo cp Scripts/interfaces /etc/network/interfaces 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 exit 0
