a9051314ea0acae6aeedbe2de26b5a7beb22852c
Ammar Qammaz <ammarkov@gmail.com>
2011-11-12 17:12:15 +0200

    added a small script to relay messages to web interface

--- Scripts/include_guarddog_to_system_startup.sh
+++ Scripts/include_guarddog_to_system_startup.sh
@@ -16,24 +16,14 @@ sudo chmod 755 /etc/init.d/guarddog_linux_startup_script
 sudo chmod +x /etc/init.d/guarddog_linux_startup_script
 sudo update-rc.d guarddog_linux_startup_script defaults 
 
-
-
-
-echo "The next segment prepares dhcp server for a 192.168.1.0-255 range  ..!"
-echo
-echo -n "                Do you want to proceed (Y/N)?"
-read answer
-if test "$answer" != "Y" -a "$answer" != "y";
-then exit 0;
-fi
+#Add a guarddog command that will relay to the RoboVision process
+sudo cp Scripts/guarddog /bin/guarddog
  
-
-sudo apt-get install dhcp3-server  
-sudo cp Scripts/dhcpd.conf /etc/dhcp3/dhcpd.conf
 
 
 echo "The next segment gives guarddog an IP of 192.168.1.14 and gateway 192.168.1.1 on ethernet ..!"
-echo "And puts it on a wireless access point mode !"
+echo "and an IP of 192.168.2.14 and gateway 192.168.2.1 on wifi ..!"
+echo "( also puts it on a wireless access point mode ! ) "
 echo
 echo -n "                Do you want to proceed (Y/N)?"
 read answer
